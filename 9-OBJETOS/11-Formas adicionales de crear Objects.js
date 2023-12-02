d66dedc9c7ec123f3727f53891bfc999f06847ed
NotGedomi <xiadouofficial@gmail.com>
2023-12-02 01:56:18 -0500

    Update Objects

---
+++ 9-OBJETOS/11-Formas adicionales de crear Objects.js
@@ -0,0 +1,12 @@
+// Formas de crear Objecs
+
+let miObjeto1 = new Object(); // Opcion Formal
+let miObjeto2 = {}; // Opcion Abreviada
+
+// Strings
+let miCadena1 = new String('Hola'); // Opcion Formal
+let miCadena2 = 'Hola'; // Opcion Abreviada
+
+// Numeros
+let miNumero1 = new Number(1); // Opcion Formal
+let miNumero2 = new Number(2); // Opcion Abreviada
