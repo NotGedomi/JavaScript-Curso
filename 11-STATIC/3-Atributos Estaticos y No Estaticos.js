b8c67e4dd3fa21c6d8e81960fd281340d826b2e7
NotGedomi <xiadouofficial@gmail.com>
2023-12-11 23:16:58 -0500

    update

--- 11-STATIC/3-Atributos Estaticos y No Estaticos.js
+++ 11-STATIC/3-Atributos Estaticos y No Estaticos.js
@@ -1,10 +1,10 @@
 // Atributos Estaticos VS No Estaticos en JS
 
 class Persona {
-    // Con Static, el atributo se asocia a la clase, mas no a ningun objeto
+    // Atributo estatico / Con Static, el atributo se asocia a la clase, mas no a ningun objeto
     static contadorObjetosPersona = 0;
 
-    // Sin Static, el atributo se asocia a los objetos
+    // Atributo no estatico / Sin Static, el atributo se asocia a los objetos
     email = 'Valor Default'; 
 
     constructor(nombre,apellido){
@@ -17,6 +17,6 @@ class Persona {
 // Impresion y prueba del atributo static
 let persona1 = new Persona('Gedomi', 'Yamete');
 console.log(persona1.contadorObjetosPersona); // Resultado: undefined <- ya que el atributo static debe usarse con la clase no con el objeto
-console.log(Persona.contadorObjetosPersona)// Resultado: 1 <- ahora si brinda el valor del atributo
+console.log(Persona.contadorObjetosPersona) // Resultado: 1 <- ahora si brinda el valor del atributo
 
 
